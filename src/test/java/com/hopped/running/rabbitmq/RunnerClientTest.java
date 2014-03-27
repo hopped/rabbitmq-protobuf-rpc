281dcc5b953dfeda5763ff57b93ce4800468d286
Dennis Hoppe <hoppe.dennis@ymail.com>
2014-03-27 14:18:12 +1300

    * changed copyright notice * organised imports * added gradle eclipse plugin

--- src/test/java/com/hopped/running/rabbitmq/RunnerClientTest.java
+++ src/test/java/com/hopped/running/rabbitmq/RunnerClientTest.java
@@ -1,29 +1,40 @@
-/*
- * Copyright (C) 2014 Dennis Hoppe. All Rights Reserved.
+/*!
+ * Copyright (c) 2014 Dennis Hoppe
+ * www.dennis-hoppe.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package com.hopped.running.rabbitmq;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.same;
-import static org.mockito.Matchers.isNull;
 
-import com.rabbitmq.client.ConnectionFactory;
-import com.hopped.running.rabbitmq.RunnerClient;
-
-import org.junit.Test;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.rabbitmq.client.ConnectionFactory;
+
 /**
  * @author Dennis Hoppe <hoppe.dennis@ymail.com>
- * @version $Id:$
  */
 @RunWith(MockitoJUnitRunner.class)
 public class RunnerClientTest {
